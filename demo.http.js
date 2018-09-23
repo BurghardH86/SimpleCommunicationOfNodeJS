865bc18f7758e7c49dee75a2326f4655038d5bf6
BurghardH <burghard.hillig@googlemail.com>
2018-09-23 17:14:51 +0200

    Added a demo to reade files from computer

--- demo.http.js
+++ demo.http.js
@@ -1,8 +1,12 @@
 var http = require('http');
+var url = require('url');
 
 // create a server object
 http.createServer(function(req, res){
+	// The first argument of the res.writeHead() method is the status code, 
+	// 200 means that all is OK, the second argument is an object containing the response headers.
 	res.writeHead(200,{'Content-Type': 'text/html'});
-	res.write('Hello World'); //write a response to the client
-	res.end(); //end the response
+	var q = url.parse(req.url, true).query;
+	var txt = q.year + " " + q.month;
+	res.end(txt); //end the response
 }).listen(8080); //server listens to port 8080
