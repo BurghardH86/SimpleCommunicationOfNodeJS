52bfed3e3fe9ac265d6c96af646bab104f6a5820
BurghardH <burghard.hillig@googlemail.com>
2018-09-23 16:55:06 +0200

    Added first own module.

--- hello-world-server.js
+++ hello-world-server.js
@@ -1,6 +1,10 @@
+// To include a module, use the require() function with the name of the module:
 var http = require('http');
+var dt = require('./myfirstmodule');
+// Now your application has access to the HTTP module, and is able to create a server:
 http.createServer(function handler(req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World\n');
+    res.write("The date and time are currently :" + dt.myDateTime() + "/n");
+    res.end();
 }).listen(1337, '127.0.0.1');
 console.log('Server running at http://127.0.0.1:1337/');
